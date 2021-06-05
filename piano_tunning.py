5badeb836c1b2d41a1ea04fa0c9374f87a641659
nadavnave <57562882+nadavnave@users.noreply.github.com>
2021-06-05 12:54:42 +0300

    working on read data

--- piano_tunning.py
+++ piano_tunning.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import argparse
 
-telephone_col_name = "Tel number"
+telephone_col_name = "Tel"
 
 parser = argparse.ArgumentParser(description='create excel of phone numbers from last year that were not called to this year')
-parser.add_argument('last_year_filename', type=str, help="The excel file name of phone numbers and names of from last year. Columns must be [Name, Tel number]")
-parser.add_argument('cur_year_filename', type=str, help="The excel file name of phone numbers and names of from current year. Columns must be [Name, Tel number]")
+parser.add_argument('last_year_filename', type=str, help="The excel file name of phone numbers and names of from last year. Columns must contain {}".format(telephone_col_name))
+parser.add_argument('cur_year_filename', type=str, help="The excel file name of phone numbers and names of from current year. Columns must contain {}".format(telephone_col_name))
 parser.add_argument('result_filename', type=str, help="The excel filename of phone numbers and names output file")
 
 args =parser.parse_args()
@@ -17,20 +17,20 @@ print( "*** Reading {} and {} excel files".format(last_year_filename, cur_year_f
 last_year_df = pd.read_excel(last_year_filename)
 cur_year_df  = pd.read_excel(cur_year_filename)
 print("---- Last year excel ----")
-#print(last_year_df)
+print(last_year_df)
 
 print("---- Current year excel ----")
-#print(cur_year_df)
+print(cur_year_df)
 
 
 last_year_tel_numbers = set(last_year_df[telephone_col_name].tolist())
 cur_year_tel_numbers = cur_year_df[telephone_col_name].tolist()
 
-result_df = pd.DataFrame(columns=['Name', 'Tel number'])
+result_df = pd.DataFrame()
 print("*** Finding not duplicated numbers")
 for number in last_year_tel_numbers:
     if number not in cur_year_tel_numbers:
-        result_df = result_df.append(last_year_df[last_year_df['Tel number']==number])
+        result_df = result_df.append(last_year_df[last_year_df[telephone_col_name]==number])
 
 print("------ The result excel is -------")
 print(result_df)
